6f508604271d7b6d04125a48a7abbd64431a946b
J. W. Kim <brecn@snu.ac.kr>
2019-02-18 22:49:15 +0900

    Add backtracking-based subset.

--- Subset/main.cc
+++ Subset/main.cc
@@ -4,6 +4,18 @@ using namespace std;
 
 char set[NUM] = { 'a', 98, 99, 'd', 'e' };
 
+int n;
+int a[NUM];
+
+void printSubset() {
+	for (int i = 1; i <= n; i++) {
+		if (a[i]) {
+			cout << set[i - 1] << " ";
+		}
+	}
+	cout << endl;
+}
+
 void allSubsetBit(int num/*number of elements in a set*/) {
 	for (int i = 0; i < 1 << num; i++) {
 		for (int j = 0; j < num; j++) {
@@ -15,10 +27,25 @@ void allSubsetBit(int num/*number of elements in a set*/) {
 	}
 }
 
-int main(void) {
-	int num_element = /*num*/3;
+void subset(int k/*level*/) {
+	if (k == n) {
+		printSubset();
+		return;
+	}
+	k++;
+	a[k] = 1;
+	subset(k);
+	a[k] = 0;
+	subset(k);
+}
 
-	allSubsetBit(num_element);
+int main(void) {
+	int num_element = 5;
+	n = num_element;
+
+	//allSubsetBit(num_element);
+
+	subset(0);
 	
 	return 0;
 }

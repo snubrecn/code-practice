8adb553fed1316e1ddfd4905a695ce7d72ec8da5
Jinwon Kim <brecn@snu.ac.kr>
2021-01-08 23:05:07 +0900

    Rewrite fibonacci
    Signed-off-by: Jinwon Kim <brecn@snu.ac.kr>

--- Fibonacci/main.cc
+++ Fibonacci/main.cc
@@ -14,54 +14,47 @@ void MeasureExecutionTime(int n, std::function<void(int n)> execution) {
   std::cout << "Execution time: " << (end - start).count() * 1e-9 << " secs\n\n";
 }
 
+void Flush() {
+  for (int i = 0; i < MAX; ++i) fibonacci[i] = 0;
+}
+
 int FibonacciRecursive(int n) {
-  if (n == 0 || n == 1) {
-    return 1;
-  }
+  if (n == 0 || n == 1) return 1;
   return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
 }
 
 int FibonacciBottomUp(int n) {
-  if (n == 0 || n == 1) {
-    return 1;
-  }
-  int fibonacci[n];
-
+  if (fibonacci[n]) return fibonacci[n];
   fibonacci[0] = fibonacci[1] = 1;
-
-  for (int i = 2; i < n; i++) {
-    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-  }
-  return fibonacci[n - 1] + fibonacci[n - 2];
+  for (int i = 2; i <= n; ++i) fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  return fibonacci[n];
 }
 
 int FibonacciTopDown(int n) {
-  if (n == 0 || n == 1) {
-    fibonacci[n] = 1;
-    return 1;
-  }
-  if (fibonacci[n]) {
-    return fibonacci[n];
-  }
+  if (fibonacci[n]) return fibonacci[n];
+  if (n == 0 || n == 1) return 1;
   fibonacci[n] = FibonacciTopDown(n - 1) + FibonacciTopDown(n - 2);
   return fibonacci[n];
 }
 
 int main(void) {
-  int seq_num;
+  int sequence;
+
   while (1) {
-    std::cin >> seq_num;
-    MeasureExecutionTime(seq_num, [](int n) {
+    std::cin >> sequence;
+    MeasureExecutionTime(sequence, [](int n) {
       std::cout << "Recursion\n";
       std::cout << "Fibonacci[" << n << "]: " << FibonacciRecursive(n) << std::endl;
     });
 
-    MeasureExecutionTime(seq_num, [](int n) {
+    MeasureExecutionTime(sequence, [](int n) {
       std::cout << "Bottom-up dynamic programming\n";
       std::cout << "Fibonacci[" << n << "]: " << FibonacciBottomUp(n) << std::endl;
     });
 
-    MeasureExecutionTime(seq_num, [](int n) {
+    Flush();
+
+    MeasureExecutionTime(sequence, [](int n) {
       std::cout << "Top-down dynamic programming\n";
       std::cout << "Fibonacci[" << n << "]: " << FibonacciTopDown(n) << std::endl;
     });

b14b6f43cace685f06a89dcdb98e49b58881a0d1
Jinwon Kim <brecn@snu.ac.kr>
2022-01-27 20:07:30 +0900

    Add remove/free to trie
    Signed-off-by: Jinwon Kim <brecn@snu.ac.kr>

--- trie/main.cc
+++ trie/main.cc
@@ -2,32 +2,43 @@
 
 struct Trie {
     Trie() {
-        is_terminal = false;
         cnt = 0;
         for (int i = 0; i < 26; ++i) next[i] = nullptr;
     }
-    void Push(char* str) {
+    void Add(char* str) {
         if (str == nullptr) return;
         if (*str == '\0') {
-            is_terminal = true;
             cnt++;
             return;
         }
         int idx = *str - 'a';
         if (next[idx] == nullptr) next[idx] = new Trie;
-        next[idx]->Push(++str);
+        next[idx]->Add(str + 1);
     }
     Trie* Find(char* str) {
         if (str == nullptr) return nullptr;
         if (*str == '\0') {
-            if (is_terminal) return this;
+            if (cnt) return this;
             return nullptr;
         }
         int idx = *str - 'a';
-        if (next[idx]) return next[idx]->Find(++str);
+        if (next[idx]) return next[idx]->Find(str + 1);
         return nullptr;
     }
-    bool is_terminal;
+    void Remove(char *str) {
+        if (str == nullptr) return;
+        if (*str == '\0') {
+            cnt = 0;
+            return;
+        }
+        int idx = *str - 'a';
+        if (next[idx]) next[idx]->Remove(str + 1);
+    }
+    void Free() {
+        for (int i = 0; i < 26; ++i)
+            if (next[i]) next[i]->Free();
+        delete this;
+    }
     int cnt;
     Trie* next[26];
 };
@@ -40,15 +51,16 @@ int main(void) {
                        "debt", "debt", "debt", "debts",
                        "abcdd", "bcde", "dbs", "bced"};
 
-    for (int i = 0; i < 16; ++i) root->Push(str_arr[i]);
+    for (int i = 0; i < 16; ++i) root->Add(str_arr[i]);
 
-    char str[10] = "bcde";
+    char str[10] = "bcds";
 
     Trie* p = root->Find(str);
 
     if (p == nullptr) std::cout << str << " does not exist\n";
     else std::cout << p->cnt << " " << str << " exist\n";
 
+    root->Free();
     return 0;
 }
 
